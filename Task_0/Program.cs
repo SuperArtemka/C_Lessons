68c1b38260545b59ac0e6f3125842a5bc55728c9
SuperArtemka <superak@bk.ru>
2023-04-24 19:59:16 +0300

    созданы две таски

---
+++ Task_0/Program.cs
@@ -0,0 +1,7 @@
+﻿// Нахождение квадрата числа
+
+Console.Write("Введите число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+int square = number * number;
+Console.Write($"Квадрат числа {number} = {square}");
---
+++ Task_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Является ли первое число квадратом второго
+
+Console.Write("Введите первое число: ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+
+if(numberOne == numberTwo * numberTwo){
+    Console.Write("Первое введенное число - это квадрат второго");
+} else {
+    Console.Write("Попробуйте снова");
+}

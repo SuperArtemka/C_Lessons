19e15f1e9f1f2cf4314268dab62ce3aa8a63174d
SuperArtemka <superak@bk.ru>
2023-04-25 20:18:39 +0300

    Day's

---
+++ Task_2/Program.cs
@@ -0,0 +1,36 @@
+﻿// Вывод дня недели по номеру
+
+Console.Write("Введите число от 1 до 7 :");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+if(dayNumber == 1){
+    Console.Write("Это понедельник");
+}
+
+else if(dayNumber == 2){
+    Console.Write("Это вторник");
+}
+
+else if(dayNumber == 3){
+    Console.Write("Это среда");
+}
+
+else if(dayNumber == 4){
+    Console.Write("Это четверг");
+}
+
+else if(dayNumber == 5){
+    Console.Write("Это пятница");
+}
+
+else if(dayNumber == 6){
+    Console.Write("Это суббота");
+}
+
+else if(dayNumber == 7){
+    Console.Write("Это воскресение");
+}
+else
+{
+    Console.Write("Этому числу не соответствует ни один день недели!");
+}

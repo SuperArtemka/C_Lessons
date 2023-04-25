5689df265134bd82b4c3b154b9c8cee446783824
SuperArtemka <superak@bk.ru>
2023-04-25 23:29:43 +0300

    последняя цифра трехзначного  числа

---
+++ Task_4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Нахождение последней цифры трехзначного числа
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int lastNumber = 0;
+
+if(number > 99 && number < 1000){
+    lastNumber = number % 10;
+    Console.Write($"Последняя цифра в {number} равна {lastNumber}");
+} 
+else
+{
+    Console.Write("Это не трехзначное число");
+}

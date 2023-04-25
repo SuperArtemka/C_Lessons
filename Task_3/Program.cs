6027e5d3414feabdb41be5399a39e6aafa8eb12c
SuperArtemka <superak@bk.ru>
2023-04-25 20:57:51 +0300

    Диапазон чисел

---
+++ Task_3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Выведение диапазона целых чисел от указанного до отрицательного
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int count = -N;
+
+while(count <= N){
+    Console.Write($"{count}");
+    count++;
+}

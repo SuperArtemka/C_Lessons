8bb3143754fa7001c367f871ec0179e7027b54ae
SuperArtemka <superak@bk.ru>
2023-04-26 23:00:32 +0300

    Удаление второй цифры трехзначного числа

---
+++ Task_6/Program.cs
@@ -0,0 +1,9 @@
+﻿// Удаление второй цифры трехзначного числа
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число: {number}");
+int digit1 = number / 100;
+int digit3 = number % 10;
+int result = digit1 * 10 + digit3;
+
+Console.WriteLine($"После удаления второй цифры останется: {result}");

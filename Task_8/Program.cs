d642b6c6604ff7b2625cefc42f5d1e00e2f33070
SuperArtemka <superak@bk.ru>
2023-04-26 23:56:27 +0300

    кратность двум числам

--- Task_8/Program.cs
+++ Task_8/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Кратность одновременно двум числам
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 7 == 0 && number % 23 == 0){
+
+Console.Write("Кратно");
+}
+else{
+Console.Write("Некратно");
+}

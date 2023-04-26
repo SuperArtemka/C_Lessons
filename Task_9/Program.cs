05335195485f26584760835f2ba9aa5fc159465d
SuperArtemka <superak@bk.ru>
2023-04-27 00:20:05 +0300

    Является ли одно число квадратом второго

---
+++ Task_9/Program.cs
@@ -0,0 +1,16 @@
+﻿// Является ли одно число квадратом второго
+Console.Write("Введите первое число: ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+
+if (numberOne == Math.Pow(numberTwo, 2) || numberTwo == Math.Pow(numberOne, 2))
+{
+    Console.Write("Да");
+}
+else
+{
+    Console.Write("Нет");
+}
+

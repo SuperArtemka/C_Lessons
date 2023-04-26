1dada0d61f76d3a43fd4bfef27c8977f84a5f9ab
SuperArtemka <superak@bk.ru>
2023-04-26 23:25:40 +0300

    Кратность чисел

---
+++ Task_7/Program.cs
@@ -0,0 +1,16 @@
+﻿// Кратно ли второе число первому
+
+Console.Write("Введите первое число: ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+
+int mod = numberOne % numberTwo;
+
+if(mod==0){
+Console.Write("Кратно");
+}
+else{
+Console.Write($"Остаток: {mod}");
+}

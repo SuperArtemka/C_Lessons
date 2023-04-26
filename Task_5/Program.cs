27155e644e4eac8b08386dffeff4cd7eac04df4e
SuperArtemka <superak@bk.ru>
2023-04-26 21:59:46 +0300

    Определение наибольшей цифры рандомного  числа

---
+++ Task_5/Program.cs
@@ -0,0 +1,14 @@
+﻿// Определение наибольшей цифры рандомного числа
+
+int number  = new Random().Next(10, 100);
+Console.WriteLine($"Случайное число:{number} ");
+
+int digit1 = number / 10;
+int digit2 = number % 10;
+
+if(digit1 > digit2){
+    Console.WriteLine($"Первая цифра числа {number} больше и равна:{digit1}");
+}
+else{
+    Console.WriteLine($"Вторая цифра числа {number} больше и равна{digit2}");
+}

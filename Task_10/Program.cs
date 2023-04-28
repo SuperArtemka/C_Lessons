4ab100fac31e9047202924e7085d1ddd94cd1b73
SuperArtemka <superak@bk.ru>
2023-04-28 19:01:24 +0300

    определение четверти по координатам

---
+++ Task_10/Program.cs
@@ -0,0 +1,62 @@
+﻿// Определение четверти по координатам точки 
+void QuatersFind(int x, int y)
+{
+    if (x > 0 && y > 0)
+    {
+        Console.WriteLine("Точка расположена в первой четверти.");
+    }
+    else if (x < 0 && y > 0)
+    {
+        Console.WriteLine("Точка расположена во второй четверти.");
+    }
+
+    else if (x < 0 && y < 0)
+    {
+        Console.WriteLine("Точка расположена в третьей четверти.");
+    }
+
+    else
+    {
+        Console.WriteLine("Точка расположена в четвертой четверти.");
+    }
+}
+
+Console.WriteLine("Введите координаты Х: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+QuatersFind(x, y);
+
+/*
+string QuatersFindWithReturn(int x, int y)
+{
+    result = " ";
+    if (x > 0 && y > 0)
+    {
+        result = "Точка расположена в первой четверти.";
+    }
+    else if (x < 0 && y > 0)
+    {
+        result = "Точка расположена во второй четверти.";
+    }
+
+    else if (x < 0 && y < 0)
+    {
+        result = "Точка расположена в третьей четверти.";
+    }
+
+    else
+    {
+        result = "Точка расположена в четвертой четверти.";
+    }
+    return result;
+}
+
+Console.WriteLine("Введите координаты Х: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+string quater = QuatersFindWithReturn(x, y);
+Console.WriteLine(quater);
+*/
+

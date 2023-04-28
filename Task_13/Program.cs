9dab5cc199cf16854bf56b495e6c584d6b4656b2
SuperArtemka <superak@bk.ru>
2023-04-29 02:52:06 +0300

    Возведение в квадрат числа от 1 до N

---
+++ Task_13/Program.cs
@@ -0,0 +1,18 @@
+﻿//Возведение в квадрат чисел от 1 до N
+
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= N; i++)
+{
+    System.Console.WriteLine(Math.Pow(i, 2));
+}
+
+/*Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while (count <= N)
+{
+    System.Console.WriteLine(count * count);
+    count++;
+}
+*/

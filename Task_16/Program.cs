d560925bb6e97868c9ba515e682e51e85762f0fd
SuperArtemka <superak@bk.ru>
2023-05-22 12:44:38 +0300

    Произведение чисел от 1 до А

---
+++ Task_16/Program.cs
@@ -0,0 +1,15 @@
+﻿// Произведение чисел от 1 до А
+
+int Multi(int number)
+{
+    int mult = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        mult *= i;
+    }
+    return mult;
+}
+System.Console.WriteLine("Введтите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int mult = Multi(num);
+System.Console.WriteLine($"Сумма чисел от 1 до {num} = {mult}");

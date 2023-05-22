977b2e9c9521ab8370a8ff6a6ea401a9d7526849
SuperArtemka <superak@bk.ru>
2023-05-22 12:15:00 +0300

    Количество цифр в числе

---
+++ Task_15/Program.cs
@@ -0,0 +1,30 @@
+﻿// Количество цифр в числе
+
+int Counting(int number){
+    int count = 0;
+    while(number>0){
+        number/=10;
+        count++;        
+        }
+        if (count == 0){
+            count++;
+        }
+    return count;
+}
+
+System.Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = Counting(num);
+System.Console.WriteLine($"Количество цифр в числе {num} = {count}");
+
+/*Второй вариант решения
+
+System.Console.WriteLine("Введите число: ");
+string num = Console.ReadLine();
+if(int.TryParse(num, out int number)){
+    System.Console.WriteLine(num.Length);
+}
+else{
+    System.Console.WriteLine("Ошибка! Введите число.");
+}
+*/

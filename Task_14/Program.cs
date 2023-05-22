687c47c682c4c790990a540dce51eb33e21c39f4
SuperArtemka <superak@bk.ru>
2023-05-22 11:21:07 +0300

    Сумма чисел от 1 до А

---
+++ Task_14/Program.cs
@@ -0,0 +1,15 @@
+﻿// Сумма чисел от 1 до А
+
+int Sum(int number)
+{
+    int sum = 0;
+    for (int i = 1; i <= number; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+System.Console.WriteLine("Введтите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int sum = Sum(num);
+System.Console.WriteLine($"Сумма чисел от 1 до {num} = {sum}");

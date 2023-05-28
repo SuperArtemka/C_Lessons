f16891897cb9fa4d2ce67732a1d9f8ad8a6a54a6
SuperArtemka <superak@bk.ru>
2023-05-28 09:27:47 +0300

    Сумма отрицательных и положительных элементов рандомного массива

---
+++ Task_18/Program.cs
@@ -0,0 +1,28 @@
+﻿//Сумма отрицательных и положительных элементов рандомного массива
+
+int[] FillArray(int size)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(-9, 10);
+    }
+    return arr;
+}
+System.Console.WriteLine("Введите число: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArray(length);
+System.Console.WriteLine(string.Join(",", array));
+
+int sumPositiv = 0;
+int sumNegativ = 0;
+for (int i = 0; i < array.Length; i++){
+    if(array[i]> 0){
+        sumPositiv+= array[i];
+    }
+    else {
+        sumNegativ+= array[i];
+    }
+}
+System.Console.WriteLine($"Сумма положительных чисел: {sumPositiv} Сумма отрицательных чисел: {sumNegativ}");

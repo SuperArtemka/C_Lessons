f2a9b80c0c9b87fdbb846c82ab6d096f61459219
SuperArtemka <superak@bk.ru>
2023-05-29 17:09:57 +0300

    Количество элементов одномерного массива, значения которых лежат в отрезке [10,99].

---
+++ Task_21/Program.cs
@@ -0,0 +1,27 @@
+﻿//Количество элементов одномерного массива, значения которых лежат в отрезке [n,N].
+
+int[] FillArrayWithRandomNumber(int size)
+{
+int[] arr = new int[size];
+Random rnd = new Random();
+for (int i = 0; i < arr.Length; i++)
+{
+arr[i] = rnd.Next(1, 300);
+}
+return arr;
+}
+
+System.Console.Write("Введите размер массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArrayWithRandomNumber(length);
+System.Console.WriteLine(string.Join(", ", array));
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+if (array[i] >= 10 && array[i] <= 99)
+{
+count++;
+}
+}
+System.Console.WriteLine(count);

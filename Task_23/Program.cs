8375153a9481087e8f9be36e6b91c5705e1672f5
SuperArtemka <superak@bk.ru>
2023-06-01 22:10:57 +0300

    Разворот одномерного массива

---
+++ Task_23/Program.cs
@@ -0,0 +1,28 @@
+﻿// Разворот одномерного массива
+
+int[] FillArrayWithRandomNumber(int size)
+{
+int[] arr = new int[size];
+Random rnd = new Random();
+for (int i = 0; i < arr.Length; i++)
+{
+arr[i] = rnd.Next(-9, 10);
+}
+return arr;
+}
+
+System.Console.Write("Введите размер массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArrayWithRandomNumber(length);
+System.Console.WriteLine(string.Join(", ", array));
+
+int temp = 0;
+
+for (int i = 0; i < array.Length / 2; i++)
+{
+temp = array[i];
+array[i] = array[array.Length - i - 1];
+array[array.Length - i - 1] = temp;
+}
+
+System.Console.WriteLine(string.Join(", ", array));

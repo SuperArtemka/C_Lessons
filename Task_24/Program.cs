69de18bedd44f446d23725dabbd281baf46df8f4
SuperArtemka <superak@bk.ru>
2023-06-01 22:15:37 +0300

    Теорема о неравенстве треугольника

---
+++ Task_24/Program.cs
@@ -0,0 +1,15 @@
+﻿// Теорема о неравенстве треугольника 
+
+System.Console.WriteLine("Введите сторону A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите сторону B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите сторону C: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a < b + c && b < a + c && c < a + b)
+System.Console.WriteLine("Треуголник существует");
+else
+System.Console.WriteLine("Треуголник не существует");

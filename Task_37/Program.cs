e0f9ce0a381a53e182d1627fc7d0345b84653986
SuperArtemka <superak@bk.ru>
2023-06-10 16:33:45 +0300

    Программа, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.

---
+++ Task_37/Program.cs
@@ -0,0 +1,17 @@
+﻿// Программа, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+int Pow(int a, int b)
+{
+if (b == 0)
+{
+return 1;
+}
+return a * Pow(a, b - 1);
+}
+
+System.Console.WriteLine("Введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine(Pow(a, b));

7efe0e10a5479f66f1e671a1b4b618ae84291e35
SuperArtemka <superak@bk.ru>
2023-06-10 16:24:54 +0300

    Программа, которая выведет все натуральные числа в промежутке от 1 до N.

---
+++ Task_34/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа, которая выведет все натуральные числа в промежутке от 1 до N. 
+void RoadToN(int number)
+{
+if (number == 0)
+{
+return;
+}
+RoadToN(number - 1);
+System.Console.Write($"{number} ");
+
+}
+
+System.Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+RoadToN(number);

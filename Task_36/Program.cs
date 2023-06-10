9b075937cc62535b84f49f5652170484701b6f6f
SuperArtemka <superak@bk.ru>
2023-06-10 16:31:21 +0300

    Программа, которая будет принимать на вход число и возвращать сумму его цифр.

---
+++ Task_36/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа, которая будет принимать на вход число и возвращать сумму его цифр.
+int SumDigital(int number)
+{
+if (number == 0)
+{
+return 0;
+}
+return number % 10 + SumDigital(number / 10);
+}
+
+System.Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(SumDigital(number));
